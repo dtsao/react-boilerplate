0475a64d21cf9a30f9eebf96d3350a05e74ef7c8
Lee <pla9@cdc.gov>
2019-08-14 09:51:01 -0400

    Delete Greetings file

--- src/Greeting.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class Greeting extends Component {
-  render() {
-    return <h1>Hello {this.props.name ? this.props.name : "World"}</h1>
-  }
-}
-
-export default Greeting;
